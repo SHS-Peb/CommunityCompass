cacab468a4a5f3cf57f5b41bda82076dca3c6d31
unknown <blackskiesdancer@gmail.com>
2023-10-31 02:21:05 +1100

    initial style and routes

--- server/controllers/stories.js
+++ server/controllers/stories.js
@@ -1,5 +1,29 @@
-const getStories = async (req, res) => {
-    res.send("Community Compass")
-    }
+import mongoose from "mongoose"
+import Story from "../models/storyContent.js"
 
-export {getStories}
+const getStories = async (req, res) => {
+
+    try {
+        const story = await Story.find();
+        res.status(200).json(story);
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+const createStory = async (req, res) => {
+    const body = req.body;
+    const newStory = new Story({
+
+    });
+
+    try {
+        await newStory.save();
+        res.status(201).json(newStory);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+
+export { getStories, createStory }
